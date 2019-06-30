f9ae611194e36124c58a7e29c8f18f620783006e
Assaf Sapir <assapir@microsoft.com>
2019-06-30 17:22:14 +0300

    add prod configuration

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,15 @@
 const env = process.env.NODE_ENV || `development`;
 const config = require(__dirname + `/config/config.json`)[env];
 
+let entitiesPaths = [];
+let migrationsPaths = [];
+if (env === `production`) {
+    entitiesPaths = ["./lib/src/models/**/*.js"];
+    migrationsPaths = ["./lib/src/migrations/**/*.js"];
+} else {
+    entitiesPaths = ["./src/models/**/*.ts"];
+    migrationsPaths = ["./src/migrations/**/*.ts"];
+}
 module.exports = {
     "type": config.dialect,
     "name": "default",
@@ -8,12 +17,8 @@ module.exports = {
     "logging": config.logging || false,
     "synchronize": false,
     "migrationsRun": true,
-    "entities": [
-        "src/models/**/*.ts"
-    ],
-    "migrations": [
-        "src/migrations/**/*.ts"
-    ],
+    "entities": entitiesPaths,
+    "migrations": migrationsPaths,
     "cli": {
         "entitiesDir": "src/models",
         "migrationsDir": "src/migrations"
--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
 export const DevelopmentEnvironment: string = `development`;
 export const ProductionEnvironment: string = `production`;
 export const env: string = process.env.NODE_ENV || DevelopmentEnvironment;
-export const config = require(__dirname + `/../config/config.json`)[env];
+export const config = env === `production` ?
+                        require(__dirname + `/../../config/config.json`)[env] :
+                        require(__dirname + `/../config/config.json`)[env];
 export const prefix: string = `api`;
 export const apiVersion: string = `v1`;
 
