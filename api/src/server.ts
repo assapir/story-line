70933f067590535faa65dfdb5ed092efe4a46bd9
Assaf Sapir <assapir@microsoft.com>
2019-07-17 09:21:50 +0300

    lgos

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,7 +9,10 @@ const port = config.port || 80;
 export const app: Express = express(); // need for tests
 
 export async function startServer(): Promise<Server> {
+    console.log(`start server`);
     const connection = await createConnection();
+    console.log(`route`);
     Router.route(app, connection);
+    console.log(`listen`);
     return app.listen(port);
 }
