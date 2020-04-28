0656afb8788160ea7e376d441399c26ca6cfffb3
Assaf Sapir <meijin007@gmail.com>
2020-04-29 01:16:36 +0300

    Pass JWT at build time

--- api/src/consts.ts
+++ api/src/consts.ts
@@ -2,11 +2,16 @@ export const DevelopmentEnvironment: string = `development`;
 export const ProductionEnvironment: string = `production`;
 export const env: string = process.env.NODE_ENV || DevelopmentEnvironment;
 export const config =
-         env === ProductionEnvironment
-           ? require(__dirname + `/config/config.json`)[env]
-           : require(__dirname + `/../config/config.json`)[env];
+  env === ProductionEnvironment
+    ? require(__dirname + `/config/config.json`)[env]
+    : require(__dirname + `/../config/config.json`)[env];
 export const prefix: string = `api`;
 export const apiVersion: string = `v1`;
+
+export const jwtSecret =
+  config.env === config.ProductionEnvironment
+    ? config.jwtSecret
+    : process.env.JWT_SECRET;
 
 export const linesPath: string = `/${prefix}/${apiVersion}/lines`;
 export const usersPath: string = `/${prefix}/${apiVersion}/users`;
